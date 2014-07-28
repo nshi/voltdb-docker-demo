817cf9007ad3489d89813ee8620f6472393980f7
Ning Shi <nshi@voltdb.com>
2014-07-28 12:48:31 -0400

    Auto download voltdb and apps, then pre-compile apps before building the image.

--- build.sh
+++ build.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
+VOLTDIST="voltdb"
+PATH=`pwd`/$VOLTDIST/bin:$PATH
+example_dir=$VOLTDIST/examples
+download_path="http://github.com/VoltDB/app-%s/archive/master.tar.gz"
+
+function help() {
+    echo "Usage: ./build.sh path-to-voltdb-download.tar.gz"
+}
+
+if [ $# -ne 1 ]; then help; exit; fi
+
+# download volt and unpacks it to voltdb/ first
+rm -rf $VOLTDIST
+mkdir $VOLTDIST
+curl -sL $1 | tar xzC $VOLTDIST --strip-components 1
+
+# download all apps and compile them, apps are listed in apps.txt
+while read i; do
+    outdir=$example_dir/$i
+    mkdir $outdir
+    curl -sL `printf $download_path $i` | tar xzC $outdir --strip-components 1
+    pushd $outdir
+    ./run.sh demo-compile
+    if [ $? != 0 ]; then exit; fi
+    popd
+done < apps.txt
+
+# build the docker image
 docker build --force-rm=true -t nshi/voltdb .
